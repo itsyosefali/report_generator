dc029450fb89f2d59de6a5abc210c9a5b12fa8e8
itsyosefali <joeyxjoey123@gmail.com>
2023-07-23 09:57:13 +0200

    added the needed doctypes

---
+++ report_generator/report_generator/doctype/filter_report/filter_report.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, yosefali and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class FilterReport(Document):
+	pass
---
+++ report_generator/report_generator/doctype/generate_report_script/generate_report_script.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, yosefali and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Generate Report Script', {
+	// refresh: function(frm) {
+
+	// }
+});
